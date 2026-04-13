9ae77c8230b31a190f87d759e931f1d57e3ac840
prasad-xma <prasadmanamperi130@gmail.com>
2026-04-14 00:16:25 +0530

    create db connection

---
+++ services/auth-service/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    const connectDB = async () => {
+        try {
+            await mongoose.connect(process.env.MONGODB);
+            console.log("Connected with database");
+
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+            process.exit(1);
+        }
+    }
+}
+
+module.exports = connectDB;
