a4111a655521834f35d502f180f75c6036d9411a
prasad-xma <prasadmanamperi130@gmail.com>
2026-04-15 18:35:44 +0530

    update: ai-service

--- services/ai-service/src/controller/aiController.js
+++ services/ai-service/src/controller/aiController.js
@@ -3,7 +3,18 @@ import {GoogleGenerativeAI} from '@google/generative-ai';
 
 dotenv.config();
 
-const client = new GoogleGenerativeAI({
-  apiKey: process.env.GEMINI_API_KEY,
-});
+const geminiAI = new GoogleGenerativeAI(
+  process.env.GEMINI_API_KEY
+);
 
+export const symptomsChecker = async (req, res) => {
+    try {
+        const { symtoms } = req.body;
+
+        const model = geminiAI.getGenerativeModel({ model: "gemini-pro" });
+        
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(5000).json({ error: "Fail to generate..."});
+    }
+};
